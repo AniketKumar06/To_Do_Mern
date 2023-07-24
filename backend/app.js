34c3a34e6e006264a41323bf8fee9581d1242e70
=Amit Kumar <=Amitkumar5211@gmail.com>
2023-07-24 18:43:19 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import morgan from 'morgan';
+
+/**Create app using express */
+
+const app = express.Router();
+
+
+/** Morgan method display */
+app.use(morgan('dev'));
+
+/** Middleware */
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 import http from 'http';
+import app from './app.js';
+import color from 'colors';
+import dotenv from 'dotenv';
 
 
-const server = http.createServer();
+/** Config the dotenv enviromental file  */
+dotenv.config({
+    path: './env/config.env'
+});
 
-const PORT =
 
-    server.listen()
+try {
+    const server = http.createServer(app);
+
+    const PORT = process.env.PORT || 5000;
+
+    server.listen(PORT, () => {
+        console.log(`Server is Running on ${PORT}`.yellow.underline.bold);
+    })
+
+} catch (error) {
+    console.log("Server Error to Start");
+}
